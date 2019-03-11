b5b3e2831b2527b194b7363e31199196d5d45c8d
xuanzebin <xuanzebin@126.com>
2019-03-11 09:42:31 +0800

    修复了新建留言中的图片不能在个人中心预览的问题

--- src/components/userCard/userCard.js
+++ src/components/userCard/userCard.js
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image } from '@tarojs/components'
 import { observer, inject } from '@tarojs/mobx'
-import { AtCard, AtTag, AtModal, AtModalHeader, AtModalContent, AtModalAction } from 'taro-ui'
+import { AtCard, AtTag, AtModal } from 'taro-ui'
 import getDateDiff from '../../utils/message'
 import './userCard.scss'
 @inject('treeHoleStore')
@@ -13,8 +13,8 @@ export default class UserCard extends Component {
     constructor() {
         super(...arguments)
         this.state = {
-            urls:[],
-            modalOpen:false
+            urls: [],
+            modalOpen: false
         }
     }
     handlePicClick(e) {
@@ -24,6 +24,12 @@ export default class UserCard extends Component {
             urls: this.state.urls // 需要预览的图片http链接列表
         })
     }
+    // componentWillMount() {
+    //     if (!this.props.treeHoleStore.messageList[this.props.index]) return true
+    //     const { files } = this.props.treeHoleStore.messageList[this.props.index]
+    //     const urls = files.map((array) => array.url)
+    //     this.setState({ urls })
+    // }
     componentDidMount() {
         const urls = this.files.map((array) => array.url)
         this.setState({ urls })
@@ -32,33 +38,33 @@ export default class UserCard extends Component {
         const { treeHoleStore } = this.props
         const index = this.props.index
         const { id } = this.props.treeHoleStore.messageList[index]
-        treeHoleStore.switchPrivate(index,id)
+        treeHoleStore.switchPrivate(index, id)
     }
     onDeleteClick(e) {
         this.setState({
-            modalOpen:true
+            modalOpen: true
         })
     }
     onConfirm(e) {
-        const {treeHoleStore} = this.props
-        let id=e.target.dataset.id
-        let index=e.target.dataset.index
-        treeHoleStore.deleteMessage(id,index)
+        const { treeHoleStore } = this.props
+        let id = e.target.dataset.id
+        let index = e.target.dataset.index
+        treeHoleStore.deleteMessage(id, index)
         this.setState({
-            modalOpen:false
+            modalOpen: false
         })
     }
     onCancel() {
         this.setState({
-            modalOpen:false
+            modalOpen: false
         })
     }
     render() {
         if (!this.props.treeHoleStore.messageList[this.props.index]) return true
         const { createdAt, like, show, files, value, hideName, privateMessage, id } = this.props.treeHoleStore.messageList[this.props.index]
-        
-        this.files=files
-        let length=like.length
+
+        this.files = files
+        let length = like.length
         const picture = files.map((array) => {
             let { url, picID } = array
             return (
@@ -77,30 +83,30 @@ export default class UserCard extends Component {
             <View className='userCard'>
                 <AtCard
                     note={time}
-                    extra={length===0?'':`已获得${length}个赞`}
-                    title={show?'已发布到树洞':'正在审核'}
+                    extra={length === 0 ? '' : `已获得${length}个赞`}
+                    title={show ? '已发布到树洞' : '正在审核'}
                     thumb='https://upload-images.jianshu.io/upload_images/11958479-114cc5f2af5f6654.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
                 >
-                    {hideName?(
-                    <AtTag 
-                        name='hideName' 
-                        type='primary' 
-                        size='small'
-                        circle
-                        disabled
-                    >
-                        已匿名
-                    </AtTag>):''}
-                    {show?<AtTag 
-                        name='private' 
-                        type='primary' 
+                    {hideName ? (
+                        <AtTag
+                            name='hideName'
+                            type='primary'
+                            size='small'
+                            circle
+                            disabled
+                        >
+                            已匿名
+                    </AtTag>) : ''}
+                    {show ? <AtTag
+                        name='private'
+                        type='primary'
                         size='small'
                         circle
                         active={privateMessage}
                         onClick={this.onPrivateClick.bind(this)}
                     >
-                        {privateMessage?'取消仅自己可见':'点击仅自己可见'}
-                    </AtTag>:''}
+                        {privateMessage ? '取消仅自己可见' : '点击仅自己可见'}
+                    </AtTag> : ''}
                     <View className='mainContent'>
                         <View className='mainText'>
                             {value}
@@ -108,7 +114,7 @@ export default class UserCard extends Component {
                         <View className='picture'>
                             {picture}
                         </View>
-                        <Text 
+                        <Text
                             className='deleteButton'
                             onClick={this.onDeleteClick.bind(this)}
                             data-index={this.props.index}
@@ -122,8 +128,8 @@ export default class UserCard extends Component {
                             cancelText='取消'
                             confirmText='确认'
                             // onClose={ this.handleClose }
-                            onCancel={ this.onCancel.bind(this) }
-                            onConfirm={ this.onConfirm.bind(this) }
+                            onCancel={this.onCancel.bind(this)}
+                            onConfirm={this.onConfirm.bind(this)}
                             data-index={this.props.index}
                             data-id={id}
                         />
